c692386f3a663b8b21bfc3b1f8143a33c710750a
Mario Young <maye.co@gmail.com>
2017-07-20 17:24:12 -0400

    Update ubuntu-init-server.sh

--- ubuntu-init-server.sh
+++ ubuntu-init-server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sudo apt-get remove docker docker-engine docker.io
+sudo apt-get remove docker docker-engine docker.io --quiet --yes
 
 sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates curl software-properties-common
 
@@ -9,7 +9,7 @@ sudo sh -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-k
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-sudo apt-get update
+sudo apt-get update --quiet --yes
 
 sudo apt-get install -y docker-ce git
 
