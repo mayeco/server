9fd04295b51ff6280a7fa9e10776c426ac2e2433
Mario Young <maye.co@gmail.com>
2019-08-11 00:12:49 -0400

    Update centos-init-server.sh

--- centos-init-server.sh
+++ centos-init-server.sh
@@ -3,13 +3,22 @@ set -e
 
 sudo yum remove -y docker docker-common container-selinux docker-selinux docker-engine docker-ce
 
+sudo yum remove docker -y \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 sudo yum makecache fast
 
-sudo yum install -y docker-ce git
+sudo yum install -y git docker-ce docker-ce-cli containerd.io 
 
 sudo sh -c 'curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose'
 
