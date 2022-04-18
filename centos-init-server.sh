d9a1168eba0944c525f3899b678be6ac9e3bc01c
Mario Young <maye.co@gmail.com>
2022-04-18 15:31:59 -0400

    Update centos-init-server.sh

--- centos-init-server.sh
+++ centos-init-server.sh
@@ -20,7 +20,7 @@ sudo yum makecache fast
 
 sudo yum install -y git docker-ce docker-ce-cli containerd.io 
 
-sudo sh -c 'curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose'
+sudo sh -c 'curl -L https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose'
 
 sudo sh -c 'chmod +x /usr/bin/docker-compose'
 
